a4df147f08817531ebfdb5bc53a3531052a8f85f
gurudatt <guru1989@gmail.com>
2015-09-27 16:21:28 -0400

    Fix for disabling admin profile edit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,14 @@
 class Admin < ActiveRecord::Base
 	validates :email, :email_format => { :message => "The email address is not valid or in the wrong format" }
+
 	validates_uniqueness_of :email
 	validates :name, presence: true
 	validates :password, length: { in: 6..20 }
-	before_destroy :destroyable?
-  	def destroyable?
-    	email != "super@admin.com"
-  	end
+	before_update :superadmin?
+	before_destroy :superadmin?
+
+
+	def superadmin?
+		id!=1
+	end
 end
